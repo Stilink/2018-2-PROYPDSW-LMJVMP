338dfbe4feb8230b1dec9726b1c1414028a25019
UroBs17 <michael.preciado@mail.escuelaing.edu.co>
2018-11-04 12:43:11 -0500

    interfaz definida

--- src/main/java/PROYPDSW/samples/services/ServicesIniciativa.java
+++ src/main/java/PROYPDSW/samples/services/ServicesIniciativa.java
@@ -2,60 +2,16 @@ package PROYPDSW.samples.services;
 
 import java.util.List;
 
+import PROYPDSW.samples.entities.Iniciativa;
 import PROYPDSW.samples.entities.Perfil;
 
 public interface ServicesIniciativa {
-	/**
-	 * Crear un perfil.
-	 * @param carne
-	 * @param nombre
-	 * @param email
-	 * @param rol
-	 * @throws ExcepcionServicesIniciativa
-	 */
-	public void registarPerfil(int carne,  String nombre, String email, String rol) throws ExcepcionServicesIniciativa;
-	
-	/**
-	 * Añadir iniciativa
-	 * @param nombre
-	 * @param dscripcion
-	 * @param creador
-	 * @throws ExcepcionServicesIniciativa
-	 */
-	public void agregarIniciativa(String nombre, String dscripcion, int creador) throws ExcepcionServicesIniciativa;
-	
-	// Para el rol administrador
-	/**
-	 * Poder cambiar o asignar a un usuario un perfil (¿rol?)
-	 * @param carne
-	 * @param rol
-	 */
-	public void asignarPerfil(int carne, String rol) throws ExcepcionServicesIniciativa;
-	
-	/**
-	 * Consultar todos los usuarios registrados en la plataforma
-	 * @return
-	 */
+	public List<Iniciativa> consultarIniciativas()throws ExcepcionServicesIniciativa;
+	public List<Iniciativa> consultarIniciativasPorEstado(String estado)throws ExcepcionServicesIniciativa;
+	public List<Iniciativa> consultarIniciativasPorPalabrasClave(List<String> palabrasClave)throws ExcepcionServicesIniciativa;
+	public void agregarIniciativa(Iniciativa ini) throws ExcepcionServicesIniciativa;
+	public Perfil consultarPerfil(String email) throws ExcepcionServicesIniciativa;
 	public List<Perfil> consultarUsuarios() throws ExcepcionServicesIniciativa;
-	
-	/**
-	 * Consultar todos los usuarios con el rol solicitado
-	 * @param rol
-	 * @return
-	 */
 	public List<Perfil> consultarPerfilPorRol(String rol) throws ExcepcionServicesIniciativa;
-	
-	/**
-	 * Consultar un usuario por su carné (carnet)
-	 * @param carne
-	 * @return
-	 */
-	public Perfil consultarPerfilPorCarne(int carne) throws ExcepcionServicesIniciativa;
-	
-	/**
-	 * Consultar usuarios por nombre
-	 * @param nombre
-	 * @return
-	 */
-	public List<Perfil> consultarPerfilPorNombre(String nombre) throws ExcepcionServicesIniciativa;
+	public List<Perfil> consultarPerfilPorArea(String area) throws ExcepcionServicesIniciativa;
 }
