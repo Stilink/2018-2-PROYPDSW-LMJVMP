01cb2a1d4a536293808650556f537d2b4f4db370
luis <luis.moreno-a@mail.escuelaing.edu.co>
2018-11-06 11:18:25 -0500

    avance

--- src/main/java/PROYPDSW/view/LogginBean.java
+++ src/main/java/PROYPDSW/view/LogginBean.java
@@ -1,5 +1,23 @@
 package PROYPDSW.view;
 
-public class LogginBean extends BasePageBean{
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@SuppressWarnings("deprecation")
+@ManagedBean (name ="loginBean")
+public class LogginBean extends BasePageBean{
+	private String text;
+	public LogginBean() {
+		
+	}
+	
+	public void setText(String t) {
+		text=t;
+	}
+	public String getText() {
+		return text;
+	}
+	
 }
