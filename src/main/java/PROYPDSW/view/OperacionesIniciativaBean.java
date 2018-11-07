bce5240424358e34daeb3ba867d1ec0b63d52097
luis <luis.moreno-a@mail.escuelaing.edu.co>
2018-11-06 22:58:30 -0500

    vista simple login

--- src/main/java/PROYPDSW/view/OperacionesIniciativaBean.java
+++ src/main/java/PROYPDSW/view/OperacionesIniciativaBean.java
@@ -1,13 +1,42 @@
 package PROYPDSW.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import com.google.inject.Inject;
+
+import PROYPDSW.samples.services.ServicesIniciativa;
 
 @SessionScoped
 @SuppressWarnings("deprecation")
 @ManagedBean (name ="operacionIBean")
 public class OperacionesIniciativaBean {
+	@Inject
+	private ServicesIniciativa service;
 	private String usuario;
+	private String nameI;
+	private String description;
+	private List<String> keyWords = new ArrayList<String>();
 	
-	
+	public void setNameI(String name) {
+		nameI=name;
+	}
+	public String getNameI() {
+		return nameI;
+	}
+	public void setDescription(String desc) {
+		description=desc;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void registrarIniciativa() {
+		
+	}
+	public void addKeyWord() {
+		
+	}
 }
