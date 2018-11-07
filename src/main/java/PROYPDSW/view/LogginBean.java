2c45e0781669e28a66c11c57348b708423d36462
luis <luis.moreno-a@mail.escuelaing.edu.co>
2018-11-06 21:46:33 -0500

    vista simple login

--- src/main/java/PROYPDSW/view/LogginBean.java
+++ src/main/java/PROYPDSW/view/LogginBean.java
@@ -8,16 +8,23 @@ import javax.faces.bean.SessionScoped;
 @SuppressWarnings("deprecation")
 @ManagedBean (name ="loginBean")
 public class LogginBean extends BasePageBean{
-	private String text;
-	public LogginBean() {
+	private String userName;
+	private String userPassword;	
+	
+	public void setUserName(String name) {
+		userName=name;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserPassword(String passwd) {
+		userPassword=passwd;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}	
+	//Login
+	public void ingresar() {
 		
 	}
-	
-	public void setText(String t) {
-		text=t;
-	}
-	public String getText() {
-		return text;
-	}
-	
 }
