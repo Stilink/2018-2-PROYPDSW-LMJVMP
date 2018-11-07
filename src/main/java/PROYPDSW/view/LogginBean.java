41c81a727db54cf3e19a3cc15f1e7f4f7f4bbcb4
luis <luis.moreno-a@mail.escuelaing.edu.co>
2018-11-06 22:56:06 -0500

    vista simple login

--- src/main/java/PROYPDSW/view/LogginBean.java
+++ src/main/java/PROYPDSW/view/LogginBean.java
@@ -1,13 +1,22 @@
 package PROYPDSW.view;
 
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import com.google.inject.Inject;
+
+import PROYPDSW.samples.services.ExcepcionServicesIniciativa;
+import PROYPDSW.samples.services.ServicesIniciativa;
 
 @SessionScoped
 @SuppressWarnings("deprecation")
 @ManagedBean (name ="loginBean")
 public class LogginBean extends BasePageBean{
+	@Inject
+	private ServicesIniciativa services;
 	private String userName;
 	private String userPassword;	
 	
@@ -24,7 +33,15 @@ public class LogginBean extends BasePageBean{
 		return userPassword;
 	}	
 	//Login
-	public void ingresar() {
-		
+	public void ingresar() throws Exception{
+		try {
+			if(services.validarLogin(userName, userPassword));
+			else {
+				FacesContext.getCurrentInstance().addMessage(null,
+		                new FacesMessage("Usuario o contraseña incorrectos"));
+			}
+		} catch (ExcepcionServicesIniciativa e) {
+			throw e;
+		}
 	}
 }
---
+++ src/main/java/PROYPDSW/view/OperacionesIniciativaBean.java
@@ -0,0 +1,13 @@
+package PROYPDSW.view;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@SuppressWarnings("deprecation")
+@ManagedBean (name ="operacionIBean")
+public class OperacionesIniciativaBean {
+	private String usuario;
+	
+	
+}
