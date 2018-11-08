d35f5d57ffc610fc108f1fdda3666e3082c63be2
luis <luis.moreno-a@mail.escuelaing.edu.co>
2018-11-07 20:30:35 -0500

    ~

--- src/main/java/PROYPDSW/view/OperacionesIniciativaBean.java
+++ src/main/java/PROYPDSW/view/OperacionesIniciativaBean.java
@@ -3,8 +3,11 @@ package PROYPDSW.view;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import org.apache.ibatis.javassist.compiler.ast.Keyword;
 
 import com.google.inject.Inject;
 
@@ -21,6 +24,10 @@ public class OperacionesIniciativaBean {
 	private String description;
 	private List<String> keyWords = new ArrayList<String>();
 	
+    @PostConstruct
+    public void init() {
+    	keyWords.add("afdafa"); 
+    }
 	public void setNameI(String name) {
 		nameI=name;
 	}
@@ -36,7 +43,13 @@ public class OperacionesIniciativaBean {
 	public void registrarIniciativa() {
 		
 	}
-	public void addKeyWord() {
-		
+	public List<String> getKeyWords() {
+		return keyWords;
+	}
+	public int tam() {
+		return keyWords.size();
+	}
+	public void addKeyWord(String kwd) {
+		keyWords.add(kwd);
 	}
 }
