7e30fe838e409071e3d85d5a77a6aee96b0e8cf1
UroBs17 <michael.preciado@mail.escuelaing.edu.co>
2018-12-01 18:38:51 -0500

    estadisticas pie area

--- src/main/java/PROYPDSW/view/EstadisticasBean.java
+++ src/main/java/PROYPDSW/view/EstadisticasBean.java
@@ -1,8 +1,14 @@
 package PROYPDSW.view;
 import org.primefaces.model.chart.PieChartModel;
 import com.google.inject.Inject;
+
+import PROYPDSW.samples.entities.Iniciativa;
 import PROYPDSW.samples.services.ExcepcionServicesIniciativa;
 import PROYPDSW.samples.services.ServicesIniciativa;
+
+
+import java.util.*;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -12,20 +18,48 @@ import javax.faces.bean.ViewScoped;
 public class EstadisticasBean extends BasePageBean{
 	@Inject
 	private ServicesIniciativa service;
-	private PieChartModel model;
+	private String dataChart;
 
-	public void iniciar() {
-		model = new PieChartModel();
-		
-        model.set("Brand 1", 5);
-        model.set("Brand 2", 3);
-        model.set("Brand 3", 7);
-        model.set("Brand 4", 4);
-        model.setLegendPosition("w");
+	
+	public String getDataChart() {
+		if (dataChart == null || dataChart.trim().length() <= 0) {
+			populateData();
+			}
+		return dataChart;
+	}
+	public void setDataChart(String dataChart) {
+		this.dataChart = dataChart;
+	}
+	private void populateData() {
+		StringBuilder stringBuilder = new StringBuilder();
+		List<String> listaDeAreas= new ArrayList<String>();
+		Map <String, Integer> dic= new HashMap<String,Integer>();
+		try {
+			for(Iniciativa ini : service.consultarIniciativas()) {
+				String area=ini.getCreador().getArea();
+				if(dic.containsKey(area)) {
+					dic.put(area, dic.get(area)+1);
+				}else {
+					dic.put(area, 1);
+				}
+			}
+		} catch (ExcepcionServicesIniciativa e) {
+			e.printStackTrace();
+		}
+		listaDeAreas.addAll(dic.keySet());
+		for (String Area : listaDeAreas) {
+			stringBuilder.append("[");
+			stringBuilder.append("'");
+			stringBuilder.append(Area);
+			stringBuilder.append("'");
+			stringBuilder.append(",");
+			stringBuilder.append(Integer.toString(dic.get(Area)));
+			stringBuilder.append("]");
+			stringBuilder.append(",");
+		}
+		dataChart= stringBuilder.toString().substring(0,
+		stringBuilder.toString().length() - 1);
 	}
 	
-	  public PieChartModel getModel() {
-	      return model;
-	  }
 }
 
