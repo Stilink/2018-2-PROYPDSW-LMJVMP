0aeae580275ad819f53370f46d38ad9e0adc7891
Jeymar Vega <jeymar.vega@mail.escuelaing.edu.co>
2018-12-02 15:54:51 -0500

    ~

--- src/main/java/PROYPDSW/view/LogginBean.java
+++ src/main/java/PROYPDSW/view/LogginBean.java
@@ -77,5 +77,10 @@ public class LogginBean extends BasePageBean{
 			e.printStackTrace();
 		}
     }
+    public void reiniciar() {
+    	username = null;
+    	perfil = null;
+    	password = null;
+    }
 }
 
