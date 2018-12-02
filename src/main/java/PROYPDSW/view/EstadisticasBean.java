63e3744a772273ce14ee77319ea7eb35fa8960f0
UroBs17 <michael.preciado@mail.escuelaing.edu.co>
2018-12-02 13:53:36 -0500

    estadisticas terminadas

--- src/main/java/PROYPDSW/view/EstadisticasBean.java
+++ src/main/java/PROYPDSW/view/EstadisticasBean.java
@@ -19,8 +19,42 @@ public class EstadisticasBean extends BasePageBean{
 	@Inject
 	private ServicesIniciativa service;
 	private String dataChart;
-
+	private String dataBarChart;
 	
+	public String getDataBarChart() {
+		if (dataBarChart == null || dataBarChart.trim().length() <= 0) {
+			populateBarData();
+			}
+		return dataBarChart;
+	}
+	private void populateBarData() {
+		StringBuilder stringBuilder = new StringBuilder();
+		List <String> listaDeEstados=new ArrayList<String>();
+		listaDeEstados.add("En espera de revision");
+		listaDeEstados.add("En revision");
+		listaDeEstados.add("Proyecto");
+		listaDeEstados.add("Solucionado");
+		try {
+			for (String Estado : listaDeEstados) {
+				stringBuilder.append("[");
+				stringBuilder.append("'");
+				stringBuilder.append(Estado);
+				stringBuilder.append("'");
+				stringBuilder.append(",");
+				stringBuilder.append(Integer.toString(service.consultarIniciativasPorEstado(Estado).size()));
+				stringBuilder.append("]");
+				stringBuilder.append(",");
+			}
+			dataBarChart= stringBuilder.toString().substring(0,
+			stringBuilder.toString().length() - 1);
+		} catch (ExcepcionServicesIniciativa e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void setDataBarChart(String dataBarChart) {
+		this.dataBarChart = dataBarChart;
+	}
 	public String getDataChart() {
 		if (dataChart == null || dataChart.trim().length() <= 0) {
 			populateData();
